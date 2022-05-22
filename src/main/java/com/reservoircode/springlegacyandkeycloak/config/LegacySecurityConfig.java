d2f5a097e964c3a1db9fd60353fc75412e47bda3
Ulrich VACHON <uvachon@gmail.com>
2022-05-22 16:44:35 +0200

    Add Spring security config for legacy part
    Signed-off-by: Ulrich VACHON <uvachon@gmail.com>

---
+++ src/main/java/com/reservoircode/springlegacyandkeycloak/config/LegacySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.reservoircode.springlegacyandkeycloak.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+//@ConditionalOnProperty(value = "keycloak.enabled", havingValue = "false", matchIfMissing = true)
+public class LegacySecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private static final Logger logger = LoggerFactory.getLogger(LegacySecurityConfig.class);
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        logger.info("Configuring the legacy security layer");
+
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeRequests().anyRequest().permitAll();
+    }
+}
